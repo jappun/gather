30bb587846287e69daff18bf4de1c6b84c1a8dc5
jappun <jappun@student.ubc.ca>
2024-11-11 20:57:27 -0800

    connected to supabase

--- src/app/newevent/page.js
+++ src/app/newevent/page.js
@@ -6,11 +6,14 @@ export default function NewEvent() {
 
     <div className="min-h-screen bg-background p-12 flex items-center justify-center font-[family-name:var(--font-geist-sans)]">
     <div className="w-3/4 bg-secondary-two rounded-lg shadow-lg p-8">
-      <div className="flex flex-col text-primary text-lg font-extrabold space-y-6">
+
+      <form className="flex flex-col text-primary text-lg font-extrabold space-y-6">
+\
         <div className="space-y-2">
           <p>Title:</p>
           <input
             type="text"
+            id="event-name"
             placeholder="My Event"
             className="font-normal text-secondary-two shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
           />
@@ -20,6 +23,7 @@ export default function NewEvent() {
           <p>Host Name:</p>
           <input
             type="text"
+            id="host-name"
             placeholder="My Name"
             className="font-normal text-secondary-two shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
           />
@@ -29,6 +33,7 @@ export default function NewEvent() {
           <p>Date:</p>
           <input
             type="date"
+            id="event-date"
             placeholder="Event Date"
             onClick={(e) => e.target.showPicker()}
             className="font-normal  text-secondary-two shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -36,11 +41,13 @@ export default function NewEvent() {
         </div>
 
         <button
-      type="button"
+      type="submit"
       className="w-1/6 inline-flex justify-center rounded-md border border-transparent bg-primary px-8 py-1 text-sm font-medium text-white hover:bg-primary-two">
         Create
     </button>
-      </div>
+
+    </form>
+
     </div>
   </div>
     );
